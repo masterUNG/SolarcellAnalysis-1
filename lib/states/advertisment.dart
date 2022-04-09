77f8cff364cac792e83643328832692efc333784
ung <test@abc.com>
2022-04-10 03:34:34 +0700

    update 10apr

--- lib/states/advertisment.dart
+++ lib/states/advertisment.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:solarcellanalysis/utility/my_constant.dart';
 
 class Advertistment extends StatelessWidget {
-  const Advertistment({ Key? key }) : super(key: key);
+  const Advertistment({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: const Text('Advertistment'),
+        backgroundColor: Colors.white,
+        foregroundColor: Myconstant.dark,
+      ),
       body: const Text('ads'),
     );
   }
--- lib/states/enviromments.dart
+++ lib/states/enviromments.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:solarcellanalysis/utility/my_constant.dart';
 
 class Environments extends StatelessWidget {
-  const Environments({ Key? key }) : super(key: key);
+  const Environments({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: const Text('Environments'),
+        backgroundColor: Colors.white,
+        foregroundColor: Myconstant.dark,
+      ),
       body: const Text('Environments'),
     );
   }
