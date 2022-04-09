3e5ece3cfc2f5127a768ebb7d5eb2354fb590b2b
ung <test@abc.com>
2022-04-10 03:31:52 +0700

    update

--- lib/states/mlm.dart
+++ lib/states/mlm.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:solarcellanalysis/utility/my_constant.dart';
 
 class Mlm extends StatelessWidget {
-  const Mlm({ Key? key }) : super(key: key);
+  const Mlm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: const Text('MLM'),
+        backgroundColor: Colors.white,
+        foregroundColor: Myconstant.dark,
+      ),
       body: const Text('mlm'),
     );
   }
